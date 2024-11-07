438f9e0c6ad602a9a36016680ea05316aa314ce8
VargaDoro <varga.dorothea.timea@gmail.com>
2024-11-07 08:46:09 +0100

    harmadik feladat

---
+++ feladatok.py
@@ -0,0 +1,35 @@
+import random
+
+def elso():
+    szam:int=int(input("Adj meg egy páros számot: "))
+    while(szam%2 != 0):
+        print("Páros számot adj meg!")
+        szam:int=int(input("Adj meg egy páros számot: "))
+    print("A megadott szám: ",szam)
+
+
+'''Írass ki a konzolra 13 db  [10, 150] zárt intervallumba eső véletlen számot.
+Hány 3-mal osztható van közöttük? A kiírás formátuma: „A számok között X db 3-mal osztható van!'''
+def masodik():
+    lista=[]
+    for i in range(0,13,1):
+        szam:int=int(random.random()*141+10)
+        lista.append(szam)
+    return lista
+
+def masodik_harommal(lista):
+    osszeg:int=0
+    db:int=0
+    for i in range(len(lista)):
+        osszeg+=lista[i]
+        if(osszeg%3==0):
+            print(f"A számok között {db} db 3-mal osztható van!")
+
+
+'''Írj eljárást, mely paraméterként kap egy text szöveget, és egy N számot. 
+ Amennyiben a szöveg rövidebb, mint a megadott N szám, akkor írjuk ki „Nincs N. karakter!”
+Ha hosszabb, akkor a text szövegnek az N. karakterét írjuk ki csupa nagybetűvel 3-szor! '''
+def harom(text:str, N:int):
+    
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+import feladatok
+
+print("Első feladat:")
+feladatok.elso()
+
+print("Második feladat:")
+lista=feladatok.masodik()
+print(lista)
+
+feladatok.masodik_harommal(lista)
